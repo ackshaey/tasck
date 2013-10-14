acdcbc0631cbc20e97c87cbe165a9e63dcd56ac6
Ackshaey Singh <ackshaey@gmail.com>
2013-10-14 09:11:45 -0700

    Implemented devise with users model, added signup and login

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,7 @@ Tasck::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  # TODO: Change for production
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Tasck::Application.routes.draw do
+  devise_for :users
   # get "pages/home"
   root "pages#home"
   get "about" => "pages#about" # Creates About Path
