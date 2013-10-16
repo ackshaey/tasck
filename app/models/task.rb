d117645355d888ae76fd491866bfffb3882dc680
Ackshaey Singh <ackshaey@gmail.com>
2013-10-15 23:57:31 -0700

    Added users>tasks association and migration

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,7 @@
 class Task < ActiveRecord::Base
+	# Belongs to association uses a singular form, 
+	# as this sounds grammatically correct.
+	# Need to create a new migration
+	# rails generate migration add_user_id_to_tasks user_id:integer:index
+	belongs_to :user
 end
